af7cbb65ea897c1473ed83a3f6ab30eb59420db3
Gabriel Neves <gabrielnevesoq@gmail.com>
2025-10-20 11:14:41 -0300

    backend legal

---
+++ emporio-shop/src/app/pages/home/home.page.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Supabase } from 'src/app/services/supabase/supabase';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+  providers: [Supabase]
+})
+export class HomePage implements OnInit {
+  constructor(private supabase: Supabase) { }
+  ngOnInit() { }
+
+  // GET - Produtos
+  public produtos: any[] = [];
+  async GetProdutos() {
+    const {data, error} = await this.supabase.GetProdutos();
+    if(error) {
+      console.error('Erro: ', error);
+    } else {
+      this.produtos = data;
+    }
+  }
+}
