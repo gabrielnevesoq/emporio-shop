9c73904cb493997c33af458abe5ccd62d32fda2d
Vinicius Madureira <vinimagod@gmail.com>
2025-10-20 15:15:50 -0300

    mensagem

--- emporio-shop/src/app/pages/home/home.module.ts
+++ emporio-shop/src/app/pages/home/home.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { HomePage } from './home.page';
+
+import { IonicModule } from '@ionic/angular';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
+import { HomePage } from './home.page';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    NgbModule
   ],
   declarations: [HomePage]
 })
